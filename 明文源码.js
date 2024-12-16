ec2926f60499e817ec24498b4d9f2b15bb8b963c
PenBond <136878219+PenBond@users.noreply.github.com>
2024-12-16 17:24:57 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,8 +1,7 @@
-
 import { connect } from "cloudflare:sockets";
 
-let password = '';
-let proxyIP = '';
+let password = 'MacBookAir@$8917-uIArYImSVWSpEOr1taZtujy/cQVg4CZqO95+FzTKjAWwtGmX/E38G1E61IsHW+GF';
+let proxyIP = '1.1.1.3:443';
 let sub = ''; 
 let subConverter = 'SUBAPI.fxxk.dedyn.io';// clash订阅转换后端，目前使用CM的订阅转换功能。自带虚假节点信息防泄露
 let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini"; //订阅配置文件
@@ -11,6 +10,13 @@ let subEmoji = 'true';
 let socks5Address = '';
 let parsedSocks5Address = {}; 
 let enableSocks = false;
+
+let dohURL = 'https://cloudflare-dns.com/dns-query';
+const doh = 'https://cloudflare-dns.com/dns-query'
+const dohjson = 'https://cloudflare-dns.com/dns-query'
+const contype = 'application/dns-message'
+const jstontype = 'application/dns-json'
+const r404 = new Response(null, {status: 404});
 
 let fakeUserID ;
 let fakeHostName ;
@@ -75,6 +81,7 @@ export default {
 			proxyIP = env.PROXYIP || env.proxyip || proxyIP;
 			proxyIPs = await ADD(proxyIP);
 			proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
+			dohURL = env.DNS_RESOLVER_URL || dohURL;
 
 			socks5Address = env.SOCKS5 || socks5Address;
 			socks5s = await ADD(socks5Address);
@@ -491,6 +498,47 @@ function makeReadableWebSocketStream(webSocketServer, earlyDataHeader, log) {
 		}
 	});
 	return stream;
+}
+
+async function handleRequest(request) {
+    // when res is a Promise<Response>, it reduces billed wall-time
+    // blog.cloudflare.com/workers-optimization-reduces-your-bill
+    let res = r404;
+    const { method, headers, url } = request
+    const {searchParams, pathname} = new URL(url)
+    
+    //Check path
+    if (!pathname.startsWith(path)) {
+        return r404;
+    }
+    if (method == 'GET' && searchParams.has('dns')) {
+        res = fetch(doh + '?dns=' + searchParams.get('dns'), {
+            method: 'GET',
+            headers: {
+                'Accept': contype,
+            }
+        });
+    } else if (method === 'POST' && headers.get('content-type') === contype) {
+        // streaming out the request body is optimal than awaiting on it
+        const rostream = request.body;
+        res = fetch(doh, {
+            method: 'POST',
+            headers: {
+                'Accept': contype,
+                'Content-Type': contype,
+            },
+            body: rostream,
+        });
+    } else if (method === 'GET' && headers.get('Accept') === jstontype) {
+        const search = new URL(url).search
+         res = fetch(dohjson + search, {
+            method: 'GET',
+            headers: {
+                'Accept': jstontype,
+            }
+        });
+    }
+    return res;
 }
 
 async function remoteSocketToWS(remoteSocket, webSocket, retry, log) {
